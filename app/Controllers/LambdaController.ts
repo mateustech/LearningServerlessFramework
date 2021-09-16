880bda99351dee6756b22e106886e4fb7dab2eda
mateustech <mateustech686@gmail.com>
2021-09-16 18:19:34 -0300

    fix: delete all methods and create method generic

--- app/Controllers/LambdaController.ts
+++ app/Controllers/LambdaController.ts
@@ -1,27 +1,13 @@
 import { Request, Response } from 'express'
 
 class LambdaController {
-    public async index(req: Request, res: Response) {
+    public async generic(req: Request, res: Response) {
       res.status(200).json({
         url: req.url,
         method: req.method.toLowerCase(),
-      });
-    }
-
-    public async store(req: Request, res: Response) {
-      res.status(200).json({
-        url: req.url,
-        method: req.method.toLowerCase(),
-        body: req.body
-      });
-    }
-
-    public async update(req: Request, res: Response) {
-      res.status(200).json({
-        url: req.url,
-        method: req.method.toLowerCase(),
-        params: req.params,
+        message: "Success",
         body: req.body,
+        querys: req.query
       });
     }
 }
